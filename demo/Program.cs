f5b0ffbc33b409ede5bbe59cf4789121a652c760
bharathmighty248 <bharathmighty248@gmail.com>
2021-07-19 09:54:14 +0530

    [bharath] Add;add and sub methods implemented

---
+++ demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num1, Num2;
+            Console.WriteLine("please enter the Num1");
+            Num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter the Num2");
+            Num2 = Convert.ToInt32(Console.ReadLine());
+            int add, sub;
+            add = Num1 + Num2;
+            sub = Num1 - Num2;
+            Console.WriteLine("sum of two numbers:" + add.ToString());
+            Console.WriteLine("Diff of two numbers:" + sub.ToString());
+        }
+    }
+}
